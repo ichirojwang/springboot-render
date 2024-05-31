002c577485ccedf412a62fab3f7ba741a6a91f7b
Ichiro Wang <ichiwang27@gmail.com>
2024-05-31 00:50:31 -0700

    init push

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,62 @@
+// var n = window.prompt("name?", "Bobby")
+// console.log("hello " + n)
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(i)
+// }
+
+// console.log(i)
+
+
+var person = {
+    name: 'bobby', 
+    age: 42, 
+    email: "random@email.com", 
+    address: {
+        number: '123', 
+        street: 'fake st'
+    },
+    speak: function(words) {
+        console.log(words)
+    }
+}
+
+// var a = 9
+
+// function myFunc() {
+//     console.log('hello world')
+//     console.log(a)
+// }
+
+// var myFunc2 = function() {
+//     console.log('hello world 2')
+// }
+
+// myFunc2()
+
+// callbacks
+
+// function asc(a, b) {
+//     return a-b
+// }
+
+// var i = 0;
+// var inter = window.setInterval(function() {
+//     console.log(i)
+//     i++
+// }, 1000)
+
+// person.speak("hello")
+
+// window.addEventListener('keypress', processKey)
+
+// function processKey(evt) {
+//     console.log(evt.key)
+// }
+
+// var button = document.querySelector('input[value="SEND"]')
+// button.addEventListener('click', function(evt) {
+//     evt.preventDefault()
+    
+// })
+    
